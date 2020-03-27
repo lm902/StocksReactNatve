827a0da3b7f8354a5cd763a50d760a0910abee37
Eric Bourne <ericbourne6371@gmail.com>
2020-03-26 17:52:35 -0700

    changed name of stockesAPI to GetAPI

--- stockapp/fetchFunc.js
+++ stockapp/fetchFunc.js
@@ -1,14 +1,13 @@
-
-  async function profileAndQuote (param) {
-    const qs = Object.entries(param).map(e => `${encodeURIComponent(e[0])}=${encodeURIComponent(e[1])}`).join('&')
-    const pr = [
-      window.fetch(`${BASE}quote?${qs}`),
-      window.fetch(`${BASE}stock/profile?${qs}`)
-    ].map(p => p.then(x => x.json()))
-    const r = await Promise.all(pr)
-    return Object.assign({}, ...r)
-  }
-  await profileAndQuote({
-      token: 'bpo09nfrh5ra872e0oi0',
-      symbol: 'AAPL'
-  }) (edited) 
+// async function profileAndQuote (param) {
+//   const qs = Object.entries(param).map(e => `${encodeURIComponent(e[0])}=${encodeURIComponent(e[1])}`).join('&')
+//   const pr = [
+//     window.fetch(`${BASE}quote?${qs}`),
+//     window.fetch(`${BASE}stock/profile?${qs}`)
+//   ].map(p => p.then(x => x.json()))
+//   const r = await Promise.all(pr)
+//   return Object.assign({}, ...r)
+// }
+// await profileAndQuote({
+//     token: 'bpo09nfrh5ra872e0oi0',
+//     symbol: 'AAPL'
+// }) (edited)
