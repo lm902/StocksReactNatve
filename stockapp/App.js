dd25b9b41f797c64febde563205f1bc2af778d6a
Eric Bourne <ericbourne6371@gmail.com>
2020-03-26 21:40:31 -0700

    Cleaned up some code in App.js to make it run

--- stockapp/App.js
+++ stockapp/App.js
@@ -1,12 +1,10 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-import Buy from "./src/components/buySell/Buy.js"
+import Buy from "./src/components/buySell/Buy.js";
 // import GetAPI from "./src/api/GetAPI.js";
-// import Login from "./components/authentication/Login";
-// import Register from "./components/authentication/Register";
-// import Portfolio from './screens/Portfolio';
-// import Detail from './screens/Detail';
+import Login from "./src/components/authenication/Login.js";
+import Register from "./src/components/authenication/Register.js";
 
 const Stack = createStackNavigator();
 
@@ -14,6 +12,8 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
+        <Stack.Screen name="Register" component={Register} />
+        <Stack.Screen name="Login" component={Login} />
         <Stack.Screen name="Buy" component={Buy} />
       </Stack.Navigator>
     </NavigationContainer>
