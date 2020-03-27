af3ea13d1bbd6c5f0b00757f2507af48551bd70e
carrotcorn <47840684+carrotcorn@users.noreply.github.com>
2020-03-27 11:07:41 -0700

    Revert "Athena"

--- stockapp/App.js
+++ stockapp/App.js
@@ -1,21 +1,29 @@
 import React from 'react';
+import axios from 'axios';
 import { StyleSheet, Text, View } from 'react-native';
-import AppNavigator from './src/nav/AppNavigation';
 
-function App() {
-  return (
-    <AppNavigator />
-  );
+export default class App extends React.Component {
+  state = {
+    stocks:[]
 }
 
-export default App;
+componentDidMount(){
+var axios.get(`https://finnhub.io/api/v1/stock/profile?symbol=AAPL&token=bpo09nfrh5ra872e0oi0`)
+.then(res=>{
+    // const stocks = res.data;
+    // this.setstate({stocks});
+    console.log(res.data);
+    });
+}
+render() {
+  return(
+      <div>
+        tets
+     
+      </div>
+          
+      
+  )
+}
+}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    color: '#444'
-  },
-});
