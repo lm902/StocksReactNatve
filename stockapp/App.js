76aff6e9330b9d8e0e5a783b3b9bc731845d00ce
Athena <athenakozak@gmail.com>
2020-03-26 17:22:41 -0700

    installed navigation, stack nav

--- stockapp/App.js
+++ stockapp/App.js
@@ -1,29 +1,42 @@
 import React from 'react';
-import axios from 'axios';
+//import axios from 'axios';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-export default class App extends React.Component {
-  state = {
-    stocks:[]
+// export default class App extends React.Component {
+//   state = {
+//     stocks:[]
+// }
+
+// componentDidMount(){
+// var axios.get(`https://finnhub.io/api/v1/stock/profile?symbol=AAPL&token=bpo09nfrh5ra872e0oi0`)
+// .then(res=>{
+//     // const stocks = res.data;
+//     // this.setstate({stocks});
+//     console.log(res.data);
+//     });
+// }
+
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+    </View>
+  );
 }
 
-componentDidMount(){
-var axios.get(`https://finnhub.io/api/v1/stock/profile?symbol=AAPL&token=bpo09nfrh5ra872e0oi0`)
-.then(res=>{
-    // const stocks = res.data;
-    // this.setstate({stocks});
-    console.log(res.data);
-    });
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
-render() {
-  return(
-      <div>
-        tets
-     
-      </div>
-          
-      
-  )
-}
-}
+
+export default App;
 
