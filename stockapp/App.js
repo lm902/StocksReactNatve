a1afa56fe34b74fb7893962fb72374ec1c23b8c0
Eric Bourne <ericbourne6371@gmail.com>
2020-03-26 18:09:08 -0700

    changed route for GetAPI to /portfolio since the portfolio will show the majority of the information along with changing the import route

--- stockapp/App.js
+++ stockapp/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Login from "./components/Login";
 import Register from "./components/Register";
-import GetAPI from "./components/GetAPI";
+import GetAPI from "./api/GetAPI";
 export default function App() {
   return (
     <Router>
