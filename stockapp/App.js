42c6b50ad276d2a1e9714f6327585abd671b3765
Eric Bourne <ericbourne6371@gmail.com>
2020-03-26 18:25:52 -0700

    updated import routes for login/register

--- stockapp/App.js
+++ stockapp/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import Login from "./components/Login";
-import Register from "./components/Register";
+import Login from "./components/authentication/Login";
+import Register from "./components/authentication/Register";
 import GetAPI from "./api/GetAPI";
 export default function App() {
   return (
