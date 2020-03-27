1d26b253d301d232e0956777158a35d759e0babd
Eric Bourne <ericbourne6371@gmail.com>
2020-03-27 00:33:32 -0700

    added the route the the app.js. other routes are commented out so i could test my own since none of the pages are connected yet

--- stockapp/App.js
+++ stockapp/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import Buy from "./src/components/buySell/Buy.js";
+import Sell from "./src/components/buySell/Sell.js";
 // import GetAPI from "./src/api/GetAPI.js";
 // import Login from "./src/components/authenication/Login.js";
 // import Register from "./src/components/authenication/Register.js";
@@ -15,6 +16,7 @@ export default function App() {
         {/* <Stack.Screen name="Register" component={Register} /> */}
         {/* <Stack.Screen name="Login" component={Login} /> */}
         <Stack.Screen name="Buy" component={Buy} />
+        <Stack.Screen name="Sell" component={Sell} />
       </Stack.Navigator>
     </NavigationContainer>
   );
