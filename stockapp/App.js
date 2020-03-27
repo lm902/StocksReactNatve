9ec3aa036a391db7c359ec7c111a45650c481419
Eric Bourne <ericbourne6371@gmail.com>
2020-03-26 18:27:03 -0700

    deleted buySell component

--- stockapp/App.js
+++ stockapp/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Login from "./components/authentication/Login";
 import Register from "./components/authentication/Register";
+
 import GetAPI from "./api/GetAPI";
 export default function App() {
   return (
