9da821e354308328fbefb86be7f471d5f48473f4
Athena <athenakozak@gmail.com>
2020-03-27 09:52:56 -0700

    added Eric's files to my branch

--- stockapp/App.js
+++ stockapp/App.js
@@ -1,23 +1,6 @@
 import React from 'react';
-//import axios from 'axios';
 import { StyleSheet, Text, View } from 'react-native';
 import AppNavigator from './src/nav/AppNavigation';
-
-// export default class App extends React.Component {
-//   state = {
-//     stocks:[]
-// }
-
-// componentDidMount(){
-// var axios.get(`https://finnhub.io/api/v1/stock/profile?symbol=AAPL&token=bpo09nfrh5ra872e0oi0`)
-// .then(res=>{
-//     // const stocks = res.data;
-//     // this.setstate({stocks});
-//     console.log(res.data);
-//     });
-// }
-
-// For testing Nav Purposes
 
 function App() {
   return (
